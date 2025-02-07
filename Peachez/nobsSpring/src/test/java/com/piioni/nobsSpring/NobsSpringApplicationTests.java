636aa2850593c8b26b2eb35539d2c23d9df92326
piioni <rangelc.juane@gmail.com>
2025-02-07 02:17:37 +0100

    Peachez spring initial commit

---
+++ Peachez/nobsSpring/src/test/java/com/piioni/nobsSpring/NobsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piioni.nobsSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
